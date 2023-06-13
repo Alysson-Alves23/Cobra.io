69ead74d4f172760c2e339deba6a72daf2b3bb34
Alysson-Alves23 <alyssonja23@gmail.com>
2023-06-13 17:02:18 -0300

    Animation define fps and initial move

--- main.cpp
+++ main.cpp
@@ -23,9 +23,10 @@
 
 #define COLUMNS 40
 #define ROWS 40
-
+#define FPS 18
 void display_callback();
 void reshape_callback(int,int);
+void timer_callback(int);
 void init();
 int main (int argc, char **v){
     glutInit(&argc, v);
@@ -34,13 +35,20 @@ int main (int argc, char **v){
     glutCreateWindow("cobra.io");
     glutDisplayFunc(display_callback);
     glutReshapeFunc(reshape_callback);
+    glutTimerFunc(0,timer_callback,0);
     init();
     glutMainLoop();
 }
+int index = 0;
 void display_callback(){
     glClear(GL_COLOR_BUFFER_BIT);
     drawGrid();
-    glutSwapBuffers();
+    glColor3f(0.8, 0.4, 0.2);  // Marrom claro
+    glRectd(index,20,index+1,21);
+    index++;
+    if(index>40){
+        index = 0;
+    } glutSwapBuffers();
 }
 void reshape_callback(int w, int h){
     glViewport(0,0,(GLsizei)w,(GLsizei) h);
@@ -49,7 +57,11 @@ void reshape_callback(int w, int h){
     glOrtho(0,COLUMNS,0,ROWS,-1,1);
     glMatrixMode(GL_MODELVIEW);
 }
+void timer_callback(int){
+    glutPostRedisplay();
+    glutTimerFunc(1000/FPS,timer_callback,0);
+}
 void init(){
-    glClearColor(0,0,0,1);
+    glClearColor(1,1,1,1);
     initGrid(COLUMNS,ROWS);
 }
