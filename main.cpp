215f4880ef8e813507e37295a76a147344e7210a
Alysson-Alves23 <alyssonja23@gmail.com>
2023-06-13 20:02:42 -0300

    Keyboard fixes

--- main.cpp
+++ main.cpp
@@ -29,6 +29,7 @@ void reshape_callback(int,int);
 void keyboard_callback(unsigned char,int,int);
 void timer_callback(int);
 void init();
+void checkPos();
 int main (int argc, char **v){
 
     glutInit(&argc, v);
@@ -45,8 +46,8 @@ int main (int argc, char **v){
 int* index = &posX;
 int direction = 1;
 void display_callback(){
-    if(*index>=40)
-        *index=0;
+    checkPos();
+
     glClear(GL_COLOR_BUFFER_BIT);
     drawGrid();
     glColor3f(0.8, 0.4, 0.2);  // Marrom claro
@@ -63,20 +64,22 @@ void reshape_callback(int w, int h){
 }
 
 void keyboard_callback(unsigned  char key, int ,int ){
-    if(key == 'w' || key == 77){
+
+    if((key == 'w' || key == GLUT_KEY_UP )&& index != &posY ){
+
         index = &posY;
         direction = 1;
     }else
-    if (key == 's'|| key == 80){
+    if ((key == 's'|| key == GLUT_KEY_DOWN) && index != &posY){
         index = &posY;
         direction = -1;
     }
     else
-    if(key == 'a'||key == 75){
+    if((key == 'a'||key == GLUT_KEY_RIGHT)&& index != &posX){
         index = &posX;
         direction = -1;
     }
-    else if(key == 'd'||key == 77){
+    else if((key == 'd'||key == GLUT_KEY_LEFT)&& index != &posX){
         index = &posX;
         direction = 1;
     }
@@ -88,4 +91,14 @@ void timer_callback(int){
 void init(){
     glClearColor(1,1,1,1);
     initGrid(COLUMNS,ROWS);
+}
+void checkPos(){
+    if(posX>=40)
+        posX=1;
+    if(posX<=0)
+        posX=39;
+    if(posY>=40)
+        posY=0;
+    if(posY<0)
+        posY=39;
 }
